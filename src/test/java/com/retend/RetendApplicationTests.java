16a1f1ba482335b70dbe5af2c8bcdff283caa6ed
tjmisko <tjmisko@users.noreply.github.com>
2023-03-04 20:27:26 -0800

    initialized repo

---
+++ src/test/java/com/retend/RetendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
